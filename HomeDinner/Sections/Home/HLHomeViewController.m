0f00d393937115d63f1646c0fdee0e0de2b46b62
houli <houli@wesai.com>
2018-02-08 14:52:32 +0800

    测试
    本地提交

--- HomeDinner/Sections/Home/HLHomeViewController.m
+++ HomeDinner/Sections/Home/HLHomeViewController.m
@@ -7,32 +7,78 @@
 //
 
 #import "HLHomeViewController.h"
+#define kHEIGHT 100
+#define IMAGEHEIGHT     200.0f
+#define MAINSCREENWIDTH     320
+@interface HLHomeViewController ()<UITableViewDelegate,UITableViewDataSource>
 
-@interface HLHomeViewController ()
-
+@property(nonatomic,strong) UITableView *tableview;
+@property (nonatomic, strong) UIImageView * headerView;
+@property(nonatomic,strong)NSArray *moreArray;
+@property(nonatomic,strong)UIImageView *zoomImageView;
 @end
 
 @implementation HLHomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-    self.view.backgroundColor = [UIColor redColor];
+    [self prepareTableview];
+    self.view.backgroundColor = [UIColor cyanColor];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)prepareTableview{
+    
+    UITableView *moreTableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    moreTableView.delegate = self;
+    moreTableView.dataSource = self;
+    moreTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:<#(nonnull NSString *)#>
+    [self.view addSubview:moreTableView];
+    
+    
+    moreTableView.contentInset = UIEdgeInsetsMake(IMAGEHEIGHT, 0, 0, 0);
+    
+    _zoomImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0,0, self.view.frame.size.width, 20)];
+    _zoomImageView.image = [UIImage imageNamed:@"IMG_0767"];
+    _zoomImageView.contentMode = UIViewContentModeScaleAspectFill;
+    [moreTableView addSubview:_zoomImageView];
+    
+    if (!_moreArray) {
+        _moreArray = [[NSArray alloc]initWithObjects:@"语言",@"体育",@"文艺",@"升学",@"职业",@"生活",@"爱好",@"1",@"2",@"3",@"4",@"5",@"6",@"7", nil];
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark -TableView Delegate and Datasource
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
 }
-*/
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _moreArray.count;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 60;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell ;
+    //static NSString *Identifier = @"Identifier";
+    cell =[tableView dequeueReusableCellWithIdentifier:nil];
+    if (cell==nil) {
+        cell=[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+        cell.textLabel.text = _moreArray[indexPath.row];
+    }
+    return cell;
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGFloat y = scrollView.contentOffset.y + 64; //如果有导航控制器，这里应该加上导航控制器的高度64
+    if (y< -IMAGEHEIGHT) {
+        CGRect frame = _zoomImageView.frame;
+        frame.origin.y = y;
+        frame.size.height = -y;
+        _zoomImageView.frame = frame;
+    }
+    
+}
 @end
